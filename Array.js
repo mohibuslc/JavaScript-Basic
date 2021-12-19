17460bfc239dead88f93f53d7891d395d9f63480
mohibuslc <mohib_uslc@outlook.com>
2021-12-19 22:00:53 +0000

    learning

--- Array.js
+++ Array.js
@@ -2,10 +2,16 @@ var mango = [13 , 12, 15, 99, 30 , 32 ];
 
 var sonali = mango [03]; // set by index ; 
 
+mango.push(27);
+
+
 mango[1]=33 // replace of digit ;
 
+var shaju = mango[6];
 
-var position = mango.indexOf(99);
+
+
+var position = mango.indexOf(32);
 
 mango.push(123); // push data to the array ;
 mango.push(188);
@@ -15,9 +21,12 @@ mango.push(188);
 mango.unshift(290); // add data beginning of Array 
 mango.shift(290) // remove Data from array beginning position ; 
 
+mango.pop(188);
+
+console.log(shaju);
 
 
-console.log(mango);
+
 
 
 
