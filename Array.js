6af5dac58c684e04880902ac26dce40a65e596ef
mohibuslc <mohib_uslc@outlook.com>
2021-12-19 21:05:05 +0000

    Basic-Javascript

---
+++ Array.js
@@ -0,0 +1,24 @@
+var mango = [13 , 12, 15, 99, 30 , 32 ]; 
+
+var sonali = mango [03]; // set by index ; 
+
+mango[1]=33 // replace of digit ;
+
+
+var position = mango.indexOf(99);
+
+mango.push(123); // push data to the array ;
+mango.push(188);
+
+
+
+mango.unshift(290); // add data beginning of Array 
+mango.shift(290) // remove Data from array beginning position ; 
+
+
+
+console.log(mango);
+
+
+
+
