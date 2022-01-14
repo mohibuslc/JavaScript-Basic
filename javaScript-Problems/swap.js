e8ebcbe054db127b2f135945e509eebcb2875e48
mohibuslc <mohib_uslc@outlook.com>
2022-01-14 21:10:09 +0000

    javascripts Problems

---
+++ javaScript-Problems/swap.js
@@ -0,0 +1,41 @@
+
+
+var a = 6 ;
+
+var b = 9 ;
+
+//console.log( " Before Swap : a = ", a , " b =", b);
+
+var temp = a;
+a = b;
+
+b= temp;
+
+//console.log(" After Swap : a = ", a , " b = ", b);
+
+// without temp use Swap ;
+
+
+var x = 5;
+
+var y = 7 ;
+
+x= x+y;
+
+y = x-y;
+
+x  = x-y ;
+
+// console.log( " After swap x = ", x , " Y = ", y);
+
+// Another Process Of Swap ;
+
+var p = 10 ;
+
+var q = 20 ;
+
+
+[p , q ] = [ q , p];
+
+console.log( " After swap P = ", p , " Q = ", q);
+
